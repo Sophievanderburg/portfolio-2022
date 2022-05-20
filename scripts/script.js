a08cbb91e5a36a34eb1d29cf889a0c0fb725c3fe
Sophie van der Burg <sophievdburg9@gmail.com>
2022-05-20 11:39:26 +0200

    typerwrite animation is workingg

--- scripts/script.js
+++ scripts/script.js
@@ -1,31 +1,53 @@
 talen = ["Hello.", "Hallo.", "Bonjour.", "Hola.", "Hallå."]
 
+const text =  document.querySelector('main div section h2')
+const button = document.querySelector('main > button')
+
 let number = 0
 
-const text =  document.querySelector('main div section h2')
+let amount = text.innerText.length
+console.log(amount)
+
+function animationOn(){
+  console.log('aan')
+  console.log(amount)
+  text.style.setProperty('animation', 'typing 2s steps( ' + amount + ', end) forwards, blink-caret 1s step-end infinite')
+}
+
+function animationOff(){
+   console.log('uit')
+  text.style.setProperty('animation', '')
+}
+
 
 function changeText (){
-  number += 1;
-  text.innerHTML = talen[number]
-   console.log('change')
-  if ( number == talen.length -1 ) {
-    number = 0
-  } 
-}
-
-function opacityAan(){
-  setTimeout(changeText, 2000)
-  console.log("aan")
-  text.style.setProperty('opacity','1')
-}
-
-function opacityUit(){
   setTimeout(()=>{
-    console.log("uit")
-    text.style.setProperty('opacity','0')
-  },1500)
-  
+    number += 1;
+    text.innerHTML = talen[number]
+    if ( number == talen.length -1 ) {
+      number = -1
+    }
+    amount = text.innerText.length
+    animationOn()
+  }, 500)
+
 }
 
-setInterval(opacityAan, 2000)
-setInterval(opacityUit, 2000)
+animationOn()
+// setInterval(animationOff, 3000)
+// setInterval(changeText, 3000)
+
+let interval1 = setInterval(animationOff, 3000)
+let interval2 = setInterval(changeText, 3000)
+
+button.addEventListener('click', ()=>{
+  console.log('click')
+  clearInterval(interval1)
+  clearInterval(interval2)
+  
+  animationOff()
+  text.innerHTML = talen[0]
+  text.style.setProperty('width','100%')
+  text.style.setProperty('border','none')
+  button.style.setProperty('display', 'none')
+})
